7980e5a3cf9c99ababd791b3518a294da010f04b
Bai Peng Peng <whyteBA@gmail.com>
2022-09-27 18:32:41 -0700

    "MostImportantCHCs" list made, using all methods of individual CHC analysis. Some useful image files added. In preparation for next report to co-authors.

--- ACD_RandomForest.R
+++ ACD_RandomForest.R
@@ -13,6 +13,7 @@ library(GGally) # for ggpairs correlation plots
 library(caTools) # for sampling the data set into training and test sets
 library(randomForest) # for the random forest algorithm
 library(dplyr) # data transforming
+library(pls) # for principle component regression
 
 ## Set WD + load data
 #setwd("F:/Users/Brian/Google Drive (ba.whyte@berkeley.edu)/R_folder/1 - NeilNSF/1 - Publication")
@@ -23,39 +24,17 @@ data <- read.csv("ModelData_byTube_Sep2022.csv")
 df1 <- subset(data, select = c(17:88)) # only individual CHCs
 bm <- data$Avg
 df2 <- df1 / t(bm)
-df <- cbind(data$LT50d, df2)
-names(df)[names(df) == 'data$LT50d'] <- 'LT50'
+df1 <- cbind(data$LT50d, df1)
+df2 <- cbind(data$LT50d, df2)
+names(df1)[names(df2) == 'data$LT50d'] <- 'LT50'
+names(df2)[names(df2) == 'data$LT50d'] <- 'LT50'
 #
 
-## BLOCK 1: Example using Iris dataset -------------------------------------------------------
-#https://www.geeksforgeeks.org/random-forest-approach-in-r-programming/
-# Load Iris
-data(iris) # loads the data set which can be called by this same name
-# Splitting data in train and test data
-split <- sample.split(iris, SplitRatio = 0.7) # randomly chooses which columns to select?
-train <- subset(iris, split == "TRUE") # 70% of iris data
-test <- subset(iris, split == "FALSE") # 30% of iris data
-
-set.seed(120)  # Setting seed. IIRC this is preferred before randomization in general
-classifier_RF = randomForest(x = train[-5], # the -5 removes the species character column
-                             y = train$Species, # because species will be the Y we try to predict?
-                             ntree = 500) # num. of trees to generate
-# Predicting the Test set results
-y_pred = predict(classifier_RF, newdata = test[-5]) # also remove species column from test dataset
-# Confusion Matrix
-confusion_mtx = table(test[, 5], y_pred) # This is similar to the confusion matrix output in classifier_RF
-# Plotting model
-plot(classifier_RF) # I guess this shows how the error levels out after enough trees generated?
-# Importance plot
-importance(classifier_RF) # For the raw importance inference
-# Variable importance plot
-varImpPlot(classifier_RF) # THIS ONE is great. A better visualization of the same importance result. 
-
-## BLOCK 2: Example but with individual CHCs to predict LT50d -------------------------------------------------------
+## BLOCK 1: Example but with individual CHCs to predict LT50d -------------------------------------------------------
 # Split the data set into training and test sets
-split <- sample.split(df, SplitRatio = 0.7)
-train <- subset(df, split == "TRUE") # 70% of data
-test <- subset(df, split == "FALSE") # 30% of data
+split <- sample.split(df1, SplitRatio = 0.7)
+train <- subset(df1, split == "TRUE") # 70% of data
+test <- subset(df1, split == "FALSE") # 30% of data
 
 set.seed(120)  # Setting seed. IIRC this is preferred before randomization in general
 reg_RF = randomForest(x = train[-1], # remove the LT50d column
@@ -81,13 +60,12 @@ for (i in 1:100) {
   t <- c(t,top$new[1:10])
   #print("Done!")
 }
-plot(as.factor(t))
-
+plot(as.factor(t), las = 2)
 
 #varImpPlot(reg_RF)
 #varImpPlot(classifier_RF, n.var=(nrow(df))) # showing all variables
 
-## BLOCK 3: Correlating individual CHCs vs. LT50 -------------------------------------------------------------------
+## BLOCK 2: Correlating individual CHCs vs. LT50 -------------------------------------------------------------------
 # Spearman correlation X vs. Y
 g1 <- ggscatter(data, x = "SumCHC", y = "Avg",
                  add = "reg.line", #conf.int = TRUE, 
@@ -104,8 +82,8 @@ ggpairs(dfp, lower = list(continuous = wrap("smooth")), upper = list(continous =
 plist <- c() # empty list to store p values from spearman tests
 clist <- c() # empty list to store correlation coefficients from spearman tests
 for (i in 1:72) {
-  y <- df$LT50 # same Y variable each time
-  x <- df[,i+1] # current CHC column
+  y <- df1$LT50 # same Y variable each time
+  x <- df1[,i+1] # current CHC column
   p <- as.numeric(cor.test(x,y,method="spearman",exact=FALSE)[3]) # p-value of current spearman test
   c <- as.numeric(cor.test(x,y,method="spearman",exact=FALSE)[4]) # p-value of current spearman test
   plist <- c(plist,p)
@@ -113,7 +91,7 @@ for (i in 1:72) {
 }
 
 # Spearman data frame using df1 CHC values (CHC mass)
-sdf1 <- tibble(colnames(df1),p.adjust(plist,method="BH"),clist)
+sdf1 <- tibble(colnames(df1)[-1],p.adjust(plist,method="BH"),clist)
 colnames(sdf1) <- c("CHC","p-adjusted","coef")
 sdf1 <- sdf1[order(sdf1$`p-adjusted`),] # lowest value is most significant
 
@@ -125,3 +103,56 @@ sdf2 <- sdf2[order(sdf2$`p-adjusted`),] # lowest value is most significant
 
 
 
+
+## BLOCK 3: PLSR loading weights to compare to random forest selection ---------------------------------------------------------
+
+## Partial least squares regression
+m1 <- plsr(LT50 ~ ., data = df1, scale = TRUE, validation = "CV") # partial least squares regression
+## Data frame for forest plot showing all loading weights in a component
+cov <- colnames(data)[17:88]
+lws <- as.numeric(m1$loading.weights[,1]) # loading weights for a component
+dfp <- data.frame(cov,lws)
+colnames(dfp) <- c("COV","LWS")
+norder <- dfp[order(dfp$LWS),][,1] # order of COV by ascending LWS
+dfp$COV <- factor(dfp$COV, levels = c(norder))
+
+## Hard threshold for loading weights (w* = median(w)/interquartile(w))
+#https://analyticalsciencejournals-onlinelibrary-wiley-com.libproxy.berkeley.edu/doi/10.1002/cem.3226
+ml <- median(dfp$LWS) # median of loading weights for Comp 1
+qr <- 0.03372973-0.15120447 # 75% minus 25% from quantile(dfp$LWS)
+w <- ml/qr # w* = median(w)/interquartile range(w)\
+
+## Manual forest plot (conf. int. plot)
+gHR <- ggplot(dfp, aes(x=COV, y=LWS)) +
+  theme_bw() +
+  #theme_black() +
+  #theme(plot.margin=unit(c(1,2,1,2),"cm")) +
+  geom_hline(yintercept=0, lty=2) +
+  #geom_errorbar(aes(ymin=LOW, ymax=UPP), color="darkgrey", width=0.25) +
+  geom_point(stat="identity", shape=1, size=3) +
+  #scale_y_continuous(position = "right") + 
+  coord_flip() +
+  ylab("") +
+  xlab("")
+#
+
+## BLOCK 4: CHC stacked bar plot ----------------------------------------------------------------------------------------
+## CHC class proportions df 
+dfb <- read.csv("CHC_classes_StackedBar.csv")
+dfb$NEST <- factor(dfb$NEST, levels = c("UK","DA","AB","LP","MT","LS","LH","SW"))
+dfb$CLASS <- factor(dfb$CLASS, levels = c("Tri","Di","Mono","Alke","Alka"))
+## Stacked bar plot - TOTAL M:L
+sb1 <- ggplot(dfb, aes(fill=CLASS, y=SUM, x=NEST)) + 
+  #theme_minimal() +
+  theme(plot.title = element_text(size = 13, hjust = 0.5)) + # Change the title font
+  labs(title="CHC class mass estimates") +
+  geom_bar(position="dodge", stat="identity") +
+  #geom_vline(aes(xintercept=3.5),linetype="dashed",size=0.5) +
+  #scale_fill_manual(values=c("#CA2E55","#341566"),name="Class",labels=c("Total Methyl alkanes","Total n-Alkanes")) +
+  #scale_fill_manual(values=c("#9E2A2B","#042A2B"),name="Class",labels=c("Total Methyl alkanes","Total n-alkanes")) +
+  #scale_fill_manual(values=c("#341566","#51E18D","#6A83F1","#ECAA46","#9E2A2B")) +
+  scale_fill_manual(values=c("#9E2A2B","#ECAA46","#6A83F1","#51E18D","darkorchid4")) +
+  #theme(legend.position="none") +
+  xlab("Nest") +
+  ylab("Mass estimate (ng) on average profile")
+#
